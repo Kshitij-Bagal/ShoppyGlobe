0b29b5d0fa425962e7e606c3a467fcbfa1ce42c3
Shivsurya <kshitijbagal@gmail.com>
2025-03-10 23:46:05 +0530

    Updates

--- assets/NotFound-D_beoZuO.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as e}from"./index-iotoG_RL.js";const n=()=>o.jsxs("div",{className:"not-found",children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"Oops! The page you’re looking for doesn’t exist."}),o.jsx(e,{to:"/ShoppyGlobe/",children:"Go back to Home"})]});export{n as default};
